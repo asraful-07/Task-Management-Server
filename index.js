a745636030154a55133a9152d59bd54d04237f19
asraful-07 <asraful0508@gmail.com>
2025-02-20 17:03:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// Middleware code
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0mh3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Define the database and Collection Campaign
+    const database = client.db("ManagementDB");
+    const tasksCollection = database.collection("tasks");
+    const userCollection = client.db("CampaignDB").collection("users");
+
+    // API route to handle POST tasks
+    app.post("/tasks", async (req, res) => {
+      const newData = req.body;
+      const result = await tasksCollection.insertOne(newData);
+      res.send(result);
+    });
+
+    // new Users related data
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // Cell to Api
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("task Management server is running");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`job task server is running on port: ${port}`);
+});
